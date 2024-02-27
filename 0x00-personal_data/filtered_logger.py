2bbdea94da63a14b7f906a7fe0f24a0e9cc105ce
dianerex <zinnyrex@yahoo.com>
2024-02-27 07:29:01 -0800

    0x00. Personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -28,7 +28,7 @@ class RedactingFormatter(logging.Formatter):
 PII_FIELDS = ("name", "email", "password", "ssn", "phone")
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
+def get_db() -> mysql.connector.connection.MYSQLConnection:
     """ Connection to MySQL environment """
     db_connect = mysql.connector.connect(
         user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
@@ -58,7 +58,7 @@ def get_logger() -> logging.Logger:
     target_handler.setLevel(logging.INFO)
 
     formatter = RedactingFormatter(list(PII_FIELDS))
-    target_handler.setFormatter(formatter)  # Corrected typo in this line
+    target_handle.setFormatter(formatter)
 
     logger.addHandler(target_handler)
     return logger
@@ -66,7 +66,7 @@ def get_logger() -> logging.Logger:
 
 def main() -> None:
     """ Obtain database connection using get_db
-    retrieve all rows in the users table and display
+    retrieve all role in the users table and display
     each row under a filtered format
     """
     db = get_db()
@@ -79,7 +79,7 @@ def main() -> None:
     for row in cursor:
         info_answer = ''
         for f, p in zip(row, headers):
-            info_answer += f'{p}={f}; '  # Removed unnecessary parentheses around 'f'
+            info_answer += f'{p}={(f)}; '
         logger.info(info_answer)
 
     cursor.close()
